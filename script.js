1b96ac70e02731d1cbaa445ccc1815084b3269d5
swnnck <samanthajamie04@gmail.com>
2026-03-26 13:22:05 -0400

    explore page

--- script.js
+++ script.js
@@ -191,12 +191,19 @@ function showResult() {
         }
     }
 
+    //this fucntion shows the results page and hides the other child pages
     const resultImage = document.getElementById("result-image");
     resultImage.src = results[personaResult].image;
     resultImage.alt = results[personaResult].title;
-
     document.getElementById("quiz").style.display = "none";
     document.getElementById("result").style.display = "block";
+
+    //this fucntion shows the explore personas page and hides the other child pages
+    document.getElementById('explore-button').addEventListener('click', function() {
+    document.getElementById('start-page').style.display = 'none';
+    document.getElementById('personas-page').style.display = 'block';
+});
+
 }
 
 
